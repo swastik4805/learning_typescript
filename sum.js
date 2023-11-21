a1726a472dfc2050c12d437fc599828bfa35cfee
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2023-11-21 20:33:55 +0530

    first commit

---
+++ sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    var res = a + b;
+    return res;
+}
+console.log(sum(12, 13));
+console.log(sum(2,6));
---
+++ sum.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b:number){
+    var res: number=a+b;
+    return res;
+}
+console.log(sum(12,13));
