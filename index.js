ef97f6d1dc722b72b94eab563f23656e7e418179
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2023-12-13 19:52:19 +0530

    added files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var arr = [1, 2, 3];
-function firstEle(arr) {
+// type numberArr=number[];
+// const arr=[1,2,3];
+function firstELe(arr) {
     return arr[0];
 }
-console.log(firstEle(arr));
+var arr1 = [1, 2, 3];
+var arr2 = ["abc", "def", "ghi"];
+console.log(firstELe(arr1));
+console.log(firstELe(arr2));
--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-type numberArr=number[];
-const arr=[1,2,3];
+// type numberArr=number[];
+// const arr=[1,2,3];
 
-function firstEle(arr: numberArr): number{
-  return arr[0];
+// function firstEle(arr: numberArr): number{
+//   return arr[0];
+// }
+// console.log(firstEle(arr));
+
+
+type numOrStr=(number| string);
+function firstELe(arr: numOrStr[]):(numOrStr){
+    return arr[0];
 }
-console.log(firstEle(arr));
+
+const arr1=[1,2,3];
+const arr2=["abc","def","ghi"];
+
+console.log(firstELe(arr1));
+console.log(firstELe(arr2));
