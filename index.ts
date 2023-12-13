6e338233ebc211e0057b1d0ec138d494e96cb7c4
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2023-12-13 20:17:19 +0530

    Update: 2023-12-13 20:17:19

--- index.ts
+++ index.ts
@@ -7,13 +7,28 @@
 // console.log(firstEle(arr));
 
 
-type numOrStr=(number| string);
-function firstELe(arr: numOrStr[]):(numOrStr){
+// type numOrStr=(number| string);
+// function firstELe(arr: numOrStr[]):(numOrStr){
+//     return arr[0];
+// }
+
+// const arr1=[1,2,3];
+// const arr2=["abc","def","ghi"];
+
+// console.log(firstELe(arr1));
+// console.log(firstELe(arr2));
+
+
+function firstELe<T>(arr:T[]){
     return arr[0];
 }
 
 const arr1=[1,2,3];
 const arr2=["abc","def","ghi"];
 
-console.log(firstELe(arr1));
-console.log(firstELe(arr2));
+const ans1=firstELe(arr1);
+const ans2=firstELe(arr2);
+
+ans2.toLowerCase();
+
+ 
